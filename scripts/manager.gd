47c2f4e9433ac8a9677f8a7d685e893b88aea68b
Marc <marc.perez.dev@gmail.com>
2025-02-01 14:40:47 -0800

    group movement

--- scripts/manager.gd
+++ scripts/manager.gd
@@ -1,16 +1,20 @@
 extends Node2D
 
-@export var selected: Array[Unit] = [  ]
+var selected: Array[Unit] = [  ]
+
 var selection_start: Vector2 = Vector2.ZERO
 
+
+# Engine
 func _process(_delta: float) -> void:
 	if selection_start != Vector2.ZERO:
 		queue_redraw()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("alt_click"):
-		for unit in selected:
-			unit.route(event.position)
+		var formation: Array[Vector2] = _get_formation(event.position)
+		for i in selected.size():
+			selected[i].route(formation[i])
 	
 	if event.is_action_pressed("click"):
 		selection_start = GameInfo.camera_offset(event.position)
@@ -43,4 +47,14 @@ func _draw() -> void:
 		var target = GameInfo.camera_offset(get_viewport().get_mouse_position())
 		draw_rect(Rect2(selection_start, target - selection_start), Color.DARK_GRAY, false, 3)
 		draw_rect(Rect2(selection_start, target - selection_start), Color(0.663, 0.663, 0.663, 0.475))
-	
+
+
+# Private
+func _get_formation(target: Vector2, spacing: int = 128) -> Array[Vector2]:
+	var formation: Array[Vector2] = [  ]
+	var columns = int(ceil(sqrt(float(selected.size()))))
+	var offset = Vector2((spacing * (columns - 1)) / 2, ((spacing / 2) * (columns - 1)) / 2)
+	for i in columns:
+		for j in columns:
+			formation.push_back(target + Vector2(j * spacing, i * spacing / 2) - offset)
+	return formation
--- scripts/unit.gd
+++ scripts/unit.gd
@@ -1,14 +1,27 @@
 extends CharacterBody2D
 class_name Unit
 
-var speed: float = 128
+var max_speed: float = 256
+var acceleration: float = 512
+var friction: float = 16
 
 @onready var nav: NavigationAgent2D = $NavigationAgent2D
 @onready var hl = $Highlight
 
 # Engine
-func _process(_delta: float) -> void:
-	position = position.move_toward(nav.get_next_path_position(), speed * _delta)
+func _process(delta: float) -> void:
+	if !nav.is_navigation_finished():
+		var direction = position.direction_to(nav.get_next_path_position())
+		velocity += direction * delta * acceleration
+		if abs(velocity.length()) > friction:
+			velocity -= direction * delta * friction
+		if abs(direction) * max_speed < abs(velocity):
+			velocity = direction * max_speed
+	else:
+		velocity = Vector2.ZERO
+	#var max_speed_vector = velocity.normalized() * max_speed
+	#if velocity.length() > max_speed_vector.length():
+		#velocity = max_speed_vector
 	move_and_slide()
 
 
